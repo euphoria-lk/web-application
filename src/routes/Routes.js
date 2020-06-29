2b3316b33b6a4cb6bfedf55230ee7e6df860d0eb
Dimuthu <kasunwpdimuthu@gmail.com>
2020-06-30 01:05:23 +0530

    routes added  for sign in patient

---
+++ src/routes/Routes.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+import {Route, Switch} from 'react-router-dom';
+// import SignUpPage from '../views/user/SignUpView';
+import SignInPage from '../views/user/SignInView';
+
+export default class Routes extends Component {
+    render() {
+        return (
+            <Switch>
+              <Route exact path="/user/login" component={SignInPage}/>
+              {/* <Route exact path="/user/signup" component={SignUpPage}/> */}
+
+            </Switch>
+        )
+    }
+}
