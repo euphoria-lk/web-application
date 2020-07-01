78fbe51b2a585c1c837b8cb522a0d9824a1f20fc
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-02 00:48:48 +0530

    refresh_token access removed

--- src/localStorageService.js
+++ src/localStorageService.js
@@ -9,17 +9,12 @@ const LocalStorageService = (function(){
  }
  function _setToken(tokenObj) {
    localStorage.setItem(‘access_token’, tokenObj.access_token);
-   localStorage.setItem(‘refresh_token’, tokenObj.refresh_token);
  }
  function _getAccessToken() {
    return localStorage.getItem(‘access_token’);
  }
- function _getRefreshToken() {
-   return localStorage.getItem(‘refresh_token’);
- }
  function _clearToken() {
    localStorage.removeItem(‘access_token’);
-   localStorage.removeItem(‘refresh_token’);
  }
   function _getUserInfo() {
     return localStorage.getItem(‘user_info’);
@@ -31,7 +26,6 @@ return {
    getService : _getService,
    setToken : _setToken,
    getAccessToken : _getAccessToken,
-   getRefreshToken : _getRefreshToken,
    clearToken : _clearToken,
    setUserInfo:_setUserInfo,
    getUserInfo:_getUserInfo
